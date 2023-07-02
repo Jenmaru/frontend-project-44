5201c42d5fd9a1d22b533961c1ba4964f78b998c
Jenmaru <1vanpersonstudio@gmail.com>
2023-07-02 23:54:12 +0300

    littlecleancodeeven

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-console.log('node bin/brain-games.js');
+console.log('brain-even');
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log('Hello, ' + name + '!');
