7460d9a02eaea5769a2fc52cd563bdf77958aee6
Jenmaru <1vanpersonstudio@gmail.com>
2023-07-02 21:28:04 +0300

    braingamescleanlint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
-import readlineSync from 'readline-sync';
 const name = readlineSync.question('May I have your name? ');
 console.log('Hello, ' + name + '!');
