bc9bf473beaf4f6ae0e63294fa5e7202909ee169
Jenmaru <1vanpersonstudio@gmail.com>
2023-07-08 16:21:30 +0300

    fixcodecalc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,61 +4,34 @@ import readlineSync from 'readline-sync';
 import  newfunc  from '../src/cli.js';
 console.log('brain-calc');
 console.log('');
-newfunc();
+const name = newfunc();
 console.log('What is the result of the expression?');
 const four = 3;
 let i = 0;
 let correct = 0;
-let q = RandomFor();
-console.log(q);
 const znak = ['-', '+', '*'];
 for (i = 0; i < four; i++) {
-  let multiplier = randomInteger(1, 100);
-  let multiply = randomInteger(1, 100);
+  let TwoNum = randomInteger(1, 100);
+  let OneNum = randomInteger(1, 100);
   let action = randomInteger(1, 3);
-  if (action == 1) {
-    console.log(multiply + ' ' + znak[0] + ' ' + multiplier);
+  let Intmass = action - 1;
+  let subtruct = OneNum - TwoNum;
+  let add = OneNum + TwoNum;
+  let multi = OneNum * TwoNum;
+  const znak2 = [subtruct, add, multi];
+    console.log(OneNum + ' ' + znak[Intmass] + ' ' + TwoNum);
     let answer = readlineSync.question('Your answer: ');
-    if (answer == (multiply - multiplier)) {
+    if (answer == znak2[Intmass]) {
         console.log('Correct!');
         correct += 1;
     }
     else {
-        let result = multiply - multiplier;
+        let result = znak2[Intmass];
         console.log("'" + answer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + result + "'" + '.');
         console.log("Let's try again, " + name + '!');
         break;
     }
   }
-  if (action == 2) {
-    console.log(multiply + ' ' + znak[1] + ' ' + multiplier);
-    let answer = readlineSync.question('Your answer: ');
-    if (answer == (multiply + multiplier)) {
-        console.log('Correct!');
-        correct += 1;
-    }
-    else {
-        let result = multiply + multiplier;
-        console.log("'" + answer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + result + "'" + '.');
-        console.log("Let's try again, " + name + '!');
-        break;
-    }
-  }
-  if (action == 3) {
-    console.log(multiply + ' ' + znak[2] + ' ' + multiplier);
-    let answer = readlineSync.question('Your answer: ');
-    if (answer == (multiply * multiplier)) {
-        console.log('Correct!');
-        correct += 1;
-    }
-    else {
-        let result = multiply * multiplier;
-        console.log("'" + answer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + result + "'" + '.');
-        console.log("Let's try again, " + name + '!');
-        break;
-    }
-  }
-}
 if (correct === 3) {
     console.log('Congratulations, ' + name + "!");
 };
