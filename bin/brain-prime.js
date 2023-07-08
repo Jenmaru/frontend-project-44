3f1ea5f22aa45b5700f65d959c41757e025c60de
Jenmaru <1vanpersonstudio@gmail.com>
2023-07-08 18:04:20 +0300

    fixing

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,44 +1,10 @@
 #!/usr/bin/env node
 import  newfunc  from '../src/cli.js';
-import readlineSync from 'readline-sync';
-import randomInteger from '../src/index.js'
+import answerFunc from '../src/index.js';
 console.log('brain-prime');
 console.log('');
 const name = newfunc();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-const four = 3;
-let i = 0;
+const evenprime = 0;
 let correctnum = 0;
-for (i = 0; i < four; i++) {
-let random = randomInteger(1, 100);
-  console.log('Question: ' + random);
-  let ansber = readlineSync.question('Your answer: ');
-  let correct = '';
-  let q = 0;
-  let dividenum = 0;
-  let division = 0;
-  for (q = 0; q <= random; q++) {
-    dividenum += 1;
-    if (random % dividenum === 0) {
-        division += 1;
-    }
-  }
-  if (division === 2) {
-    correct = 'yes';
-  }
-  if (division > 2) {
-    correct = 'no';
-  }
-  if (ansber === correct) {
-    console.log('Correct!');
-    correctnum += 1;
-}
-else { 
-  console.log("'" + ansber + "'" + " is wrong answer ;(. Correct answer was 'no'.");
-console.log("Let's try again, " + name + "!");
-break; 
-}
-if (correctnum === 3) {
-    console.log('Congratulations, ' + name + "!");
-};
-};
+answerFunc(name, correctnum, evenprime);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
-export default function randomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+import readlineSync from 'readline-sync';
+export default function answerFunc(name, correctnum, evenprime) {
+  function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  };
+    const four = 3;
+    let i = 0;
+    let q = 0;
+    for (i = 0; i < four; i++) {  
+        let random = randomInteger(1, 100);
+        console.log('Question: ' + random);
+        let answer = readlineSync.question('Your answer: ');
+        let correct = '';
+        let dividenum = 0;
+        let division = 0;
+        for (q = 0; q <= random; q++) {
+            dividenum += 1;
+            if (random % dividenum === 0) {
+                division += 1;
+            }
+          }
+          if (evenprime === 0) {
+            if (division === 2) {
+              correct = 'yes';
+            };
+            if (division > 2) {
+              correct = 'no';
+            };      
+          }
+          if (evenprime === 1) {
+        if (random % 2 === 0) {
+          correct = 'yes';
+        };
+        if (random % 2 !== 0) {
+          correct = 'no';
+        };  
+      }    
+if (answer === correct) {
+console.log('Correct!');
+correctnum += 1;
+}
+else { 
+console.log("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correct + "'" + ".");
+console.log("Let's try again, " + name + "!");
+break;
+}
+};
+if (correctnum === 3) {
+    console.log('Congratulations, ' + name + "!");
+};
 };
 
 
