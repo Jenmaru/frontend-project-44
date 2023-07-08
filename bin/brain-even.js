034ad4cede36d984d7550cd59eee75cd442e756b
Jenmaru <1vanpersonstudio@gmail.com>
2023-07-08 18:41:36 +0300

    fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,48 @@
 #!/usr/bin/env node
 import  newfunc  from '../src/cli.js';
-import answerFunc from '../src/index.js';
+import readlineSync from 'readline-sync';
+import randomInteger from '../src/index.js';
 console.log('brain-even');
 console.log('');
 const name = newfunc();
 console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
-const evenprime = 1;
 let correctnum = 0;
-answerFunc(name, correctnum, evenprime);
+const four = 3;
+    let i = 0;
+    let q = 0;
+    for (i = 0; i < four; i++) {  
+        let random = randomInteger(1, 100);
+        console.log('Question: ' + random);
+        let answer = readlineSync.question('Your answer: ');
+        let correct = '';
+        let dividenum = 0;
+        let division = 0;
+        for (q = 0; q <= random; q++) {
+            dividenum += 1;
+            if (random % dividenum === 0) {
+                division += 1;
+            }
+          }
+        if (random % 2 === 0) {
+          correct = 'yes';
+        };
+        if (random % 2 !== 0) {
+          correct = 'no';
+        }; 
+        if (answer === correct) {
+          console.log('Correct!');
+          correctnum += 1;
+          }
+          else { 
+          console.log("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correct + "'" + ".");
+          console.log("Let's try again, " + name + "!");
+          break;
+          }
+          if (correctnum === 3) {
+            console.log('Congratulations, ' + name + "!");
+          };
+    };
+
+
 
 
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,10 +1,44 @@
 #!/usr/bin/env node
 import  newfunc  from '../src/cli.js';
-import answerFunc from '../src/index.js';
+import randomInteger from '../src/index.js';
+import readlineSync from 'readline-sync';
 console.log('brain-prime');
 console.log('');
 const name = newfunc();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-const evenprime = 0;
 let correctnum = 0;
-answerFunc(name, correctnum, evenprime);
+const four = 3;
+    let i = 0;
+    let q = 0;
+    for (i = 0; i < four; i++) {  
+        let random = randomInteger(1, 100);
+        console.log('Question: ' + random);
+        let answer = readlineSync.question('Your answer: ');
+        let correct = '';
+        let dividenum = 0;
+        let division = 0;
+        for (q = 0; q <= random; q++) {
+            dividenum += 1;
+            if (random % dividenum === 0) {
+                division += 1;
+            }
+          }
+        if (division === 2) {
+          correct = 'yes';
+        };
+        if (division !== 2) {
+          correct = 'no';
+        }; 
+        if (answer === correct) {
+          console.log('Correct!');
+          correctnum += 1;
+          }
+          else { 
+          console.log("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correct + "'" + ".");
+          console.log("Let's try again, " + name + "!");
+          break;
+          }
+          if (correctnum === 3) {
+            console.log('Congratulations, ' + name + "!");
+          };
+    };
