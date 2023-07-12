992519514597fe34aa2923ad3f0e9b46355083aa
Jenmaru <1vanpersonstudio@gmail.com>
2023-07-12 09:50:10 +0300

    fix

--- src/index1.js
+++ src/index1.js
@@ -1,31 +1,16 @@
 import readlineSync from 'readline-sync';
-import randomInteger from '../src/index.js';
-export default function logic(name) {
-    let correctnum = 0;
-    const four = 3;
+export default function logic(name, correct, correctnum) {
     let i = 0;
-    for (i = 0; i < four; i++) {  
-        let random = randomInteger(1, 100);
-        console.log('Question: ' + random);
-        let correct = '';
-        if (random % 2 === 0) {
-          correct = 'yes';
-        };
-        if (random % 2 !== 0) {
-          correct = 'no';
-        }; 
+    for (i = 0; i < 1; i ++) {
     let answer = readlineSync.question('Your answer: ');
         if (answer === correct) {
           console.log('Correct!');
-          correctnum += 1;
+          return correctnum += 1;
           }
         if (answer !== correct) { 
           console.log("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correct + "'" + ".");
           console.log("Let's try again, " + name + "!"); 
-          break;
+          return false;
           }
-          if (correctnum === 3) {
-            console.log('Congratulations, ' + name + "!");
-          };
-        };
+        }
     };
