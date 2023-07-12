63488c82aed6fca7f65789495df2560cfcb7ba10
Jenmaru <1vanpersonstudio@gmail.com>
2023-07-12 15:58:26 +0300

    hexletfailure

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ import randomInteger from '../index.js';
 console.log('brain-even');
 console.log('');
 const name = newfunc();
-console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const Number = (random) => random % 2 === 0;
 const answer = () => {
 const question = randomInteger(1, 100);
